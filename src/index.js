03bfc7c6d4068dcea080e7dbd2f1ca3a319361cb
Mani Jafari <manielli@gmail.com>
2019-01-25 13:58:41 -0800

    Created a QuestionDetails tag

--- src/index.js
+++ src/index.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// A react component is a function that returns a React element.
+// You React component's must use the PascalCase naming convention.
+// Components whose name does not begin with a capital letter will
+// be considered plain HTML elements.
+const QuestionDetails = () => {
+    return (
+        <div>
+            <h2>What is your favourite colour?</h2>
+            <p>Red, green, blue, magenta, etc.</p>
+            <p>Bt Bridge Troll</p>
+            <p>
+                <small>Seen 10 time(s)</small>•<small>Create 10 days ago</small>
+                <small>Last edited 2 hours ago</small>
+            </p>
+        </div>
+    )
+}
+// In JSX, self-closing tags must be closed. <img> doesn't work, you
+// must write <img/> to close it.
+ReactDOM.render(<QuestionDetails />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
