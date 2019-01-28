8a7fcb535578acf6cb69ee253e833d156402219d
Mani Jafari <manielli@gmail.com>
2019-01-28 14:25:18 -0800

    Add question delete button from question index page

--- src/components/QuestionIndexPage.js
+++ src/components/QuestionIndexPage.js
@@ -4,9 +4,35 @@ class QuestionIndexPage extends Component {
   constructor(props) {
     super(props);
 
+    // Very temporary you should never code like this lying aroud
+    // Only to play around in the console with questions
+    window.questions = props.questions;
+
     this.state= {
       questions: [ ...this.props.questions ]
     };
+  }
+
+  deleteQuestion(id) {
+    console.log("Deleting", id);
+
+    // To change `state`, you must ALWAYS use the `this.setState()`
+    // method.
+
+    // You can use setState by passing an object as its first argument.
+    // When the time comes, the object will be merged with the current
+    // state changing whatever properties are inside.
+    // this.setState({ questions: this.state.questions.filter(q => q-id !== id) });
+
+    // You can also use setState by giving a callback as a first argument
+    // that receives the current state and props as arguments. It must
+    // an object that will be merged with the state.
+    this.setState((state, props) => ({
+      questions: state.questions.filter(q => q.id !== id )
+    }))
+
+    // More on setState:
+    // http://reactjs.org/docs/state-and-lifecycle.html
   }
 
   render () {
@@ -19,7 +45,11 @@ class QuestionIndexPage extends Component {
           }}>
               {this.state.questions.map(question => (
                 <li key={question.id}>
-                    <a href="#ignore-me">{question.title}</a>
+                    <a href="#ignore-me">{question.title}</a> <br/>
+                    <button onClick={event => {
+                      // console.log("Delete clicked!");
+                      this.deleteQuestion(question.id);
+                    }} >Delete</button>
                 </li>  
               ))}
         </ul>
