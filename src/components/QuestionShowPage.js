2987ad04d42a9922ff896781ae516fff196ff586
Mani Jafari <manielli@gmail.com>
2019-01-28 14:57:06 -0800

    Add solution for Delete question show page for one question

--- src/components/QuestionShowPage.js
+++ src/components/QuestionShowPage.js
@@ -9,12 +9,32 @@ class QuestionShowPage extends Component {
     this.state = {
       question: { ...this.props.question }
     };
+
+    this.deleteQuestion = this.deleteQuestion.bind(this);
+    // .bind() is a method of functions. It allows us to permanently
+    // set a `this` for that function.
+    // We do this to make sure that whenever deleteQuestion will be
+    // called, its `this` will refer to the instance of the component
+    // where we can `setState` amongst methods.
+  }
+
+  deleteQuestion() {
+    console.log("Deleting... question with the id");
+
+    this.setState({ question: null });
   }
 
   render () {
+    if (!this.state.question) {
+        return (
+          <h2>Question doesn't exist!</h2>
+        );
+    }
     return (
       <main>
         <QuestionDetails {...this.state.question} />
+        <button onClick={this.deleteQuestion} >Delete</button>
+
         <h2>Answers</h2>
         <AnswerList answers={this.state.question.answers} />
       </main>
